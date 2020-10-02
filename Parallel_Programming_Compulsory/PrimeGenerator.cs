49290a5314cf01974f42f422a9b3a178e4a43ab5
Dominik Nagy <32298986+NagyDominik@users.noreply.github.com>
2020-10-02 08:41:07 +0200

    rev 2

--- Parallel_Programming_Compulsory/PrimeGenerator.cs
+++ Parallel_Programming_Compulsory/PrimeGenerator.cs
@@ -40,39 +40,42 @@ namespace Parallel_Programming_Compulsory
 
         public List<long> GetPrimesParallel(long first, long last)
         {
-            List<long> resultList = new List<long>();
-            BitArray bits = new BitArray((int)last + 1, true); // All true
-            bits[0] = false;
-            bits[1] = false;
-
-            for (int i = 1; i * i <= last; i++) // Loops until the square root of the last number
-            {
-                if (bits[(int)i])
+            var locker = new Object();
+            IEnumerable<long> primes = new List<long>();
+            Parallel.ForEach(
+                Partitioner.Create(first, last),
+                () => new List<long>(),
+                (range, loopState, partialResult) =>
                 {
-                    Parallel.ForEach(Partitioner.Create(i, last), (range) =>
+                    for (long i = range.Item1; i < range.Item2; i++)
                     {
-                        BitArray temp = new BitArray((int)last + 1, true);
-                        for (long j = range.Item1 * range.Item1; j <= last; j += range.Item1) // Removes all the products of the current number.
-                        {
-                            temp[(int)j] = false;
-                        }
-                        lock (this)
-                        {
-                            bits.And(temp);
-                        }
-                    });
-                }
-            }
-
-            for (int i = 0; i < bits.Length; i++) // BitArray to List<long>
-            {
-                if (bits[i])
+                        if (IsPrime((int)i))
+                            partialResult.Add(i);
+                    }
+                    return partialResult;
+                }, (partialResult) =>
                 {
-                    resultList.Add(i);
-                }
-            }
+                    lock (locker)
+                    {
+                        primes = primes.Concat(partialResult);
+                    }
+                });
+            return primes.OrderBy(s => s).ToList();
+        }
 
-            return resultList.Where(x => x >= first).ToList();
+        private bool IsPrime(int number)
+        {
+            if (number <= 1) return false;
+            if (number == 2) return true;
+            if (number % 2 == 0) return false;
+
+            var boundary = (int)Math.Floor(Math.Sqrt(number));
+
+            for (int i = 3; i <= boundary; i += 2)
+                if (number % i == 0)
+                    return false;
+
+            return true;
         }
     }
 }
